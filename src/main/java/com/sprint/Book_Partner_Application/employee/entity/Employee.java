97e561f40a9343adce3a48231a0f3e1e0a29fb1b
sanjai-code <sanjai2004arivu@gmail.com>
2026-04-15 11:49:01 +0530

    publisher entity and employee entity added

---
+++ src/main/java/com/sprint/Book_Partner_Application/employee/entity/Employee.java
@@ -0,0 +1,59 @@
+package com.sprint.Book_Partner_Application.employee.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @Column(name = "emp_id", length = 10, nullable = false)
+    @NotBlank(message = "Employee ID is required")
+    @Pattern(
+            regexp = "^[A-Z]{3}[1-9][0-9]{4}[FM]$|^[A-Z]-[A-Z][1-9][0-9]{4}[FM]$",
+            message = "Invalid employee ID format"
+    )
+    private String empId;
+
+    @Column(name = "fname", nullable = false, length = 20)
+    @NotBlank(message = "First name is required")
+    @Size(max = 20, message = "First name must not exceed 20 characters")
+    private String fname;
+
+    @Column(name = "minit", length = 1)
+    @Size(max = 1, message = "Middle initial must be 1 character")
+    private String minit;
+
+    @Column(name = "lname", nullable = false, length = 30)
+    @NotBlank(message = "Last name is required")
+    @Size(max = 30, message = "Last name must not exceed 30 characters")
+    private String lname;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_id", nullable = false)
+    @NotNull(message = "Job is required")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Job job;
+
+    @Column(name = "job_lvl")
+    @Builder.Default
+    private Integer jobLvl = 10;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pub_id", nullable = false)
+    @NotNull(message = "Publisher is required")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Publisher publisher;
+
+    @Column(name = "hire_date", nullable = false)
+    @Builder.Default
+    private LocalDateTime hireDate = LocalDateTime.now();
+}

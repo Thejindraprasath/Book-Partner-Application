774da61c7c665f70ec4bc9a2adadfce6b4fbe14a
sachitha <tcsachithasree@gmail.com>
2026-04-16 11:05:47 +0530

    Service layer added

---
+++ src/main/java/com/sprint/Book_Partner_Application/book/service/TitleService.java
@@ -0,0 +1,29 @@
+package com.sprint.Book_Partner_Application.book.service;
+
+import com.sprint.Book_Partner_Application.book.dto.RoySchedDTO;
+import com.sprint.Book_Partner_Application.book.dto.TitleDTO;
+import com.sprint.Book_Partner_Application.dto.PageResponse;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface TitleService {
+    TitleDTO.Response createTitle(TitleDTO.Request request);
+    PageResponse<TitleDTO.Response> getAllTitles(String type, String pubId, Double minPrice, Double maxPrice, Pageable pageable);
+
+    TitleDTO.Response getTitleById(String titleId);
+
+    TitleDTO.Response updateTitle(String titleId, TitleDTO.UpdateRequest request);
+
+    void deleteTitle(String titleId);
+
+    List<AuthorDTO.Response> getAuthorsByTitle(String titleId);
+
+    List<RoySchedDTO.Response> getRoySchedsByTitle(String titleId);
+
+    RoySchedDTO.Response createRoySched(RoySchedDTO.Request request);
+
+    RoySchedDTO.Response updateRoySched(Long roySchedId, RoySchedDTO.Request request);
+
+    void deleteRoySched(Long roySchedId);
+}

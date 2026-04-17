df7f6cbc730888fa5df6924932f2b39445973bf4
vennila <vennilathanigaiarasu@gmail.com>
2026-04-17 13:04:05 +0530

    Controller Added

--- src/main/java/com/sprint/Book_Partner_Application/store/service/StoreService.java
+++ src/main/java/com/sprint/Book_Partner_Application/store/service/StoreService.java
@@ -1,31 +1,27 @@
 package com.sprint.Book_Partner_Application.store.service;
 
-//public interface StoreService {
-//}
-//package com.bookpartner.service;
-
-import com.sprint.Book_Partner_Application.dto.PageResponse;
-import com.sprint.Book_Partner_Application.store.dto.DiscountDTO;
 import com.sprint.Book_Partner_Application.dto.PageResponse;
 import com.sprint.Book_Partner_Application.sales.dto.SaleDTO;
-import com.sprint.Book_Partner_Application.store.dto.StoreDTO;
+import com.sprint.Book_Partner_Application.store.dto.request.*;
+import com.sprint.Book_Partner_Application.store.dto.response.*;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface StoreService {
-    StoreDTO.Response createStore(StoreDTO.Request request);
-    PageResponse<StoreDTO.Response> getAllStores(String city, String state, Pageable pageable);
-    StoreDTO.Response getStoreById(String storId);
-    StoreDTO.Response updateStore(String storId, StoreDTO.UpdateRequest request);
+
+    StoreResponse createStore(StoreCreateRequest request);
+    PageResponse<StoreResponse> getAllStores(String city, String state, Pageable pageable);
+    StoreResponse getStoreById(String storId);
+    StoreResponse updateStore(String storId, StoreUpdateRequest request);
     void deleteStore(String storId);
     List<SaleDTO.Response> getTransactionsByBranch(String storId);
-    List<DiscountDTO.Response> getDiscountsByBranch(String storId);
+    List<DiscountResponse> getDiscountsByBranch(String storId);
 
-    // Discount operations
-    DiscountDTO.Response createDiscount(DiscountDTO.Request request);
-    List<DiscountDTO.Response> getAllDiscounts();
-    DiscountDTO.Response getDiscountByType(String discountType);
-    List<DiscountDTO.Response> getDiscountsByBranchId(String storId);
-    DiscountDTO.Response updateDiscount(Long discountId, DiscountDTO.Request request);
+    // Discount
+    DiscountResponse createDiscount(DiscountCreateRequest request);
+    List<DiscountResponse> getAllDiscounts();
+    DiscountResponse getDiscountByType(String discountType);
+    List<DiscountResponse> getDiscountsByBranchId(String storId);
+    DiscountResponse updateDiscount(Long discountId, DiscountUpdateRequest request);
 }

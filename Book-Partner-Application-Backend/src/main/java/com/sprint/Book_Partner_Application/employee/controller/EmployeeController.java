a3f735bcae28d7507cbd6c94b448c22aa9b603ab
sanjai-code <sanjai2004arivu@gmail.com>
2026-04-25 15:39:55 +0530

    Feature/publisher employee (#94)
    * feature/publisher-employee added
    
    * Configuration Updated
    
    * config updated
    
    * config updated
    
    * publisher entity and employee entity added
    
    * Repository updated
    
    * Service and Dto Added
    
    * Service Updated
    
    * controller added
    
    * controller updated
    
    * publisher and employee exception updated
    
    * publisher and employee testcase added
    
    * Removed builder in publisher and employee
    
    * method name modified in publisher and employee test
    
    * EmployeeController Modified
    
    * Employee Entity Modified
    
    * Sale test modified
    
    * frontend publisher module added
    
    * publisher and employee module service,test modified
    
    * frontend code modified
    
    * publisher service modified
    
    * frontend modified
    
    ---------
    
    Co-authored-by: Thejindraprasath <162461834+Thejindraprasath@users.noreply.github.com>

--- Book-Partner-Application-Backend/src/main/java/com/sprint/Book_Partner_Application/employee/controller/EmployeeController.java
+++ Book-Partner-Application-Backend/src/main/java/com/sprint/Book_Partner_Application/employee/controller/EmployeeController.java
@@ -33,8 +33,8 @@ public class EmployeeController {
     }
 
     @GetMapping("/jobs")
-    public ResponseEntity<ApiResponse<List<JobResponse>>> getAllJobs() {
-        return ResponseEntity.ok(ApiResponse.success(employeeService.getAllJobs()));
+    public PageResponse<JobResponse> getAllJobs(Pageable pageable) {
+        return employeeService.getAllJobs(pageable);
     }
 
     @GetMapping("/jobs/{jobId}")
